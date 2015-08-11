bb00abfaee22f71d13f86f164d048e158db9e56d
bobson555 <bobkob@student.mini.pw.edu.pl>
2015-08-11 17:34:49 +0200

    Niewyczyszczony silnik obliczeniowy, niedokomentowany

---
+++ ExpressionTrees/ExpressionTrees/Algex.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.ComponentModel;
+
+namespace ExpressionTrees
+{
+
+    public class Algex : INotifyPropertyChanged
+    {
+        public static string[] Operators = {"(", ")", "+", "-", "*", "/"};
+
+        string _expression;
+        public string InnerExpression
+        {
+            get {return _expression;}
+            set
+            {
+                if (!IsProperArithmeticExpression(value)) throw new ArgumentException();
+                _expression = value;
+                FindAllNeededVariables();
+                toONP(InnerExpression);
+                IsSolved = false;
+                //PropertyChanged.Invoke(this, new PropertyChangedEventArgs("InnerExpression"));
+            }
+        }
+        List<string> ONP;
+
+        Dictionary<string, double> _varCtx;
+        public Dictionary<string, double> VariableContext
+        {
+            get { return _varCtx; }
+            set
+            {
+                _varCtx = value;
+                //PropertyChanged.Invoke(this, new PropertyChangedEventArgs("VariableContext"));
+            }
+        }
+
+        List<string> _dependantOn;
+        public List<string> DependantOn
+        {
+            get { return _dependantOn; }
+            private set
+            {
+                _dependantOn = value;
+                //PropertyChanged.Invoke(this, new PropertyChangedEventArgs("DependantOn"));
+            }
+        }
+
+        bool _isSolved = false;
+        public bool IsSolved
+        {
+            get { return _isSolved; }
+            private set
+            {
+                _isSolved = value;
+                //PropertyChanged.Invoke(this, new PropertyChangedEventArgs("IsSolved"));
+            }
+        }
+
+        double _value = double.NaN;
+        public double Value
+        {
+            get { return _value; }
+            private set
+            {
+                _value = value;
+                //PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Value"));
+            }
+        }
+
+        public Algex(string expression, IDictionary<string, double> variables = null, bool immediateSolve = false)
+        {
+            if (variables != null) VariableContext = (Dictionary<string, double>)variables;
+            InnerExpression = expression;
+            if (immediateSolve || DependantOn.Count == 0) Solve();
+        }
+
+        private void FindAllNeededVariables()
+        {
+            var depOn = new List<string>();
+            var rgx = new Regex(@"{\w*}");
+            var matches = rgx.Matches(InnerExpression);
+            foreach(Match match in matches)
+            {
+                var sbstr = match.Value.Substring(1, match.Value.Length - 2);
+                if(!depOn.Contains(sbstr))
+                depOn.Add(sbstr);
+            }
+            depOn.Distinct().ToList();
+            depOn.Sort();
+            DependantOn = depOn;
+        }
+
+        public bool UnknownVariablePresent
+        {
+            get
+            {
+                foreach (string varId in DependantOn)
+                {
+                    if (VariableContext != null && !VariableContext.Keys.Contains(varId)) return true;
+                }
+                return false;
+            }
+        }
+
+        public static bool IsProperArithmeticExpression(string expression)
+        {
+            //TODO: Add culture specific decimal separator and enable decimal thingies to be accepted
+            var rgx = new Regex(@"^-?((({\w+})|0|([1-9]\d*)))([+\-*/](-?(({\w+})|0|([1-9]\d*))))*$", RegexOptions.IgnorePatternWhitespace);
+            var q = new Queue<string>();
+            q.Enqueue(expression);
+            while(q.Count!=0)
+            {
+                var exp = q.Dequeue();
+                var left = exp.Contains("(");
+                var right = exp.Contains(")");
+                if (left ^ right) return false;
+                if (exp.Contains("("))
+                {
+                    int open = exp.IndexOf('(');
+                    int close = exp.IndexOf(')');
+                    if (open > close) return false;
+                    //sprawdzanie pomiędzy
+                    int n = 1;
+                    for (int i = open + 1; i < exp.Length; i++)
+                    {
+                        if (exp[i] == '(') n++;
+                        if (exp[i] == ')')
+                        {
+                            n--;
+                            if (n == 0)
+                            {
+                                var innerExp = exp.Substring(open + 1, i - open - 1);
+                                var sb = new StringBuilder();
+                                //if (open - 1 >= 0)
+                                sb.Append(exp.Substring(0, open));
+                                sb.Append("0");
+                                if (i + 1 < exp.Length)
+                                    sb.Append(exp.Substring(i+1));
+                                q.Enqueue(innerExp);
+                                q.Enqueue(sb.ToString());
+                                break;
+                            }
+                        }
+                    }
+                    if (n > 0) return false; //nawiasy otwarte, nigdy nie zamknięte
+                }
+                else
+                {
+                    if(!rgx.IsMatch(exp)) return false;
+                }
+            }
+            return true;
+        }
+
+        private void toONP(string expression)
+        {
+            var f = expression.Replace(" ", "");
+            f = f.Replace("-(", "-1*(");
+            f = f.Replace("+", " + ");
+            f = f.Replace("*", " * ");
+            f = f.Replace("/", " / ");
+            f = f.Replace("-", " - ");
+            f = f.Replace("(", " ( ");
+            f = f.Replace(")", " ) ");
+            var s = f.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries).ToList();
+            for (int i = 0; i < s.Count; i++)
+            {
+                if(s[i]=="-" && (i==0 || Operators.Contains(s[i-1])))
+                {
+                    s.RemoveAt(i);
+                    s[i] = "-"+s[i];
+                }
+            }
+            var stack = new Stack<string>();
+            var output = new List<string>();
+            for (int i = 0; i < s.Count; i++)
+            {
+                switch (s[i])
+                {
+                    case "(":
+                        stack.Push("(");
+                        break;
+                    case ")":
+                        while (stack.Peek() != "(")
+                        {
+                            output.Add(stack.Pop());
+                        }
+                        stack.Pop();
+                        break;
+                    case "+":
+                        if (stack.Count != 0)
+                        {
+                            while (stack.Count > 0 && stack.Peek() != "(")
+                            {
+                                output.Add(stack.Pop());
+                            }
+                        }
+                        stack.Push("+");
+                        break;
+                    case "-":
+                        if (stack.Count != 0)
+                        {
+                            while (stack.Count > 0 && stack.Peek() != "(")
+                            {
+                                output.Add(stack.Pop());
+                            }
+                        }
+                        stack.Push("-");
+                        break;
+                    case "*":
+                        if (stack.Count != 0)
+                        {
+                            while (stack.Count > 0 && stack.Peek() != "(" && stack.Peek() != "+" && stack.Peek() != "-")
+                            {
+                                output.Add(stack.Pop());
+                            }
+                        }
+                        stack.Push("*");
+                        break;
+                    case "/":
+                        if (stack.Count != 0)
+                        {
+                            while (stack.Count > 0 && stack.Peek() != "(" && stack.Peek() != "+" && stack.Peek() != "-" && stack.Peek() != "*" && stack.Peek() != "/")
+                            {
+                                output.Add(stack.Pop());
+                            }
+                        }
+                        stack.Push("/");
+                        break;
+                    default:
+                        output.Add(s[i]);
+                        break;
+                }
+            }
+            output.AddRange(stack);
+            ONP = output;
+        }
+
+        public bool Solve()
+        {
+            return Solve(this.VariableContext);
+        }
+
+        public bool Solve(IDictionary<string, double> variableCtx)
+        {
+            if (UnknownVariablePresent) return false;
+            var s = new Stack<double>();
+            for (int i = 0; i < ONP.Count; i++)
+            {
+                if (Operators.Contains(ONP[i]))
+                {
+                    var right = s.Pop();
+                    var left = s.Pop();
+                    double result;
+                    switch (ONP[i])
+                    {
+                        case "+": result = left + right; break;
+                        case "-": result = left - right; break;
+                        case "*": result = left * right; break;
+                        case "/": result = left / right; break;
+                        default: throw new NotSupportedException();
+                    }
+                    s.Push(result);
+                }
+                else
+                {
+                    if (ONP[i].EndsWith("}") && ONP[i].StartsWith("{"))
+                    {
+                        var var = ONP[i].Substring(1, ONP[i].Length - 2);
+                        s.Push(variableCtx[var]);
+                    }
+                    else
+                    s.Push(double.Parse(ONP[i]));
+                }
+            }
+            if (s.Count != 1) throw new ArgumentException();
+            Value = s.Pop();
+            IsSolved = true;
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return InnerExpression+" = "+Value;
+        }
+
+        public void Reset()
+        {
+            IsSolved = false;
+            Value = double.NaN;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ ExpressionTrees/ExpressionTrees/AlgexSet.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace ExpressionTrees
+{
+    public class AlgexSet:IDictionary<string, string>, INotifyPropertyChanged, INotifyCollectionChanged
+    {
+        private Dictionary<string, Algex> _dict;
+
+        public bool ImmediateSolve
+        {
+            get;
+            set;
+        }
+
+        public AlgexSet()
+        {
+            _dict = new Dictionary<string, Algex>();
+        }
+
+        public bool TrySolveAll()
+        {
+            bool anySolved = true;
+            while (anySolved)
+            {
+                anySolved = _dict.Keys
+                    .Where(x => !_dict[x].IsSolved && _dict[x].DependantOn.Intersect(GetSolvedKeys()).Count() == _dict[x].DependantOn.Count)
+                    .Aggregate(false, (current, key) => current || _dict[key].Solve(GetValues()));
+            }
+            return _dict.Count == _dict.Count(x => x.Value.IsSolved);
+        }
+
+        public void SolveAll()
+        {
+            var allVars = _dict.Aggregate((IEnumerable<string>)(new List<string>()), (current, x) => x.Value.DependantOn.Union(current)).ToList();
+            if (allVars.Intersect(_dict.Keys).Count() != allVars.Count) throw new Exception("There are undefined variables");
+            if (!TrySolveAll()) throw new Exception("Recursion Detected");
+        }
+
+        public void Add(string key, string expression)
+        {
+            var exp = new Algex(expression);
+            this.Add(key, exp);
+            if (ImmediateSolve) TrySolveAll();
+        }
+
+        private void Add(string key, Algex value)
+        {
+            _dict.Add(key, value);
+            //CollectionChanged.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add));
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return _dict.ContainsKey(key);
+        }
+
+        public ICollection<string> Keys
+        {
+            get { return _dict.Keys; }
+        }
+
+        public bool Remove(string key)
+        {
+            var toReset = GetAllDependingOn(key);
+            if (_dict.Remove(key))
+            {
+                foreach (var variable in toReset)
+                {
+                    _dict[variable].Reset();
+                }
+                //CollectionChanged.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove));
+                return true;
+            }
+            return false;
+         }
+
+        public bool TryGetValue(string key, out string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<string> Values
+        {
+            get
+            {
+                return (ICollection<string>)_dict.Select(x => x.Value.InnerExpression);
+            }
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                return _dict[key].InnerExpression;
+            }
+            set
+            {
+                _dict[key].InnerExpression = value;
+                foreach (var var in GetAllDependingOn(key))
+                {
+                    _dict[var].Reset();
+                }
+                if (ImmediateSolve) TrySolveAll();
+                //CollectionChanged.Invoke(_dict[key], new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace));
+            }
+        }
+
+        private List<string> GetAllDependingOn(string key)
+        {
+            var result = new List<string>{key};
+            var extendedResult = new List<string>{key};
+            do
+            {
+                result = result.Union(extendedResult).ToList();
+                var d = _dict.Where(x => x.Value.DependantOn.Intersect(result).Count() > 0).Select(x => x.Key).ToList();
+                extendedResult = extendedResult.Union(d).ToList();
+            }
+            while (result.Count() < extendedResult.Count());
+            result.Remove(key);
+            result.Sort();
+            return result;
+        }
+
+        public Dictionary<string, double> GetValues()
+        {
+            var result = new Dictionary<string, double>();
+            foreach (var key in _dict.Keys)
+            {
+                if (_dict[key].IsSolved)
+                    result.Add(key, _dict[key].Value);
+            }
+            return result;
+         }
+
+        public List<string> GetSolvedKeys()
+        {
+            return _dict.Where(x => x.Value.IsSolved).Select(x => x.Key).ToList();
+        }
+
+        public void RenameVariable(string oldName, string newName)
+        {
+
+            if (!_dict.ContainsKey(oldName)) throw new KeyNotFoundException();
+            if (_dict.ContainsKey(newName)) throw new ArgumentException();
+            var item = _dict[oldName];
+            foreach (var var in GetAllDependingOn(oldName))
+            {
+                    _dict[var].Reset();
+            }
+            foreach (var var in _dict.Where(x => x.Value.DependantOn.Contains(oldName)))
+            {
+                _dict[var.Key].InnerExpression = _dict[var.Key].InnerExpression.Replace("{"+oldName+"}", "{"+newName+"}"); 
+            }
+            _dict.Remove(oldName);
+            Add(newName, item.InnerExpression);
+        }
+
+        public double Value(string key)
+        {
+            return _dict[key].Value;
+        }
+
+        public void Add(KeyValuePair<string, string> item)
+        {
+            this.Add(item.Key, item.Value);
+        }
+
+        public void Clear()
+        {
+            _dict.Clear();
+            //CollectionChanged.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        public bool Contains(KeyValuePair<string, string> item)
+        {
+            return _dict[item.Key].InnerExpression == item.Value;
+        }
+
+        public void CopyTo(KeyValuePair<string, string>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { return _dict.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(KeyValuePair<string, string> item)
+        {
+            if (!Contains(item)) return false;
+            return Remove(item.Key);
+            
+        }
+
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+    }
+}
